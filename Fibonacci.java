9d3f80c320a3163862e6f84ddf39af82981d7195
Haruaki Tamada <tamada@users.noreply.github.com>
2020-12-20 10:11:56 +0900

    fixed content type of List

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,7 +1,7 @@
 public class Fibonacci{
     public void run(String[] args) {
         int max = parseMax(args);
-        List<String> list = fibonacciList(max);
+        List<Integer> list = fibonacciList(max);
         printList(list);
     }
 }
